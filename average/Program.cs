d39ca86ee36dfdcee1ed9cd3fc551db06b2df3b5
ArtKoshh <artur.koshmin@hotmail.com>
2024-08-24 20:46:43 +0200

    Add project files.

---
+++ average/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace confitionalIsAndLoops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var x = Max(1, 2);
+    }
+    static int Max(int x,int y)
+    {
+        if(x > y)
+        { return x;
+        }
+        else
+            { return y;
+        }
+    }
+    
+
+}
